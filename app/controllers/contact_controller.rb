370f7c0bb9296771bd1e6b61276880000162cbe5
Nicolas Vinet <nvinet@gmail.com>
2014-01-11 14:29:44 +0000

    removing custome

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -9,8 +9,6 @@ class ContactController < ApplicationController
     @contact.arrival = Date.civil(params[:arrival][:year].to_i, params[:arrival][:month].to_i, params[:arrival][:day].to_i)
     @contact.departure = Date.civil(params[:departure][:year].to_i, params[:departure][:month].to_i, params[:departure][:day].to_i)
     if @contact.valid?
-      @customer = Customer.where(:email => @contact.email).first_or_create(params[:contact])
-      @customer.save
       ContactMailer.enquire(@contact).deliver
       flash[:success] = true
       redirect_to :action => 'index'
