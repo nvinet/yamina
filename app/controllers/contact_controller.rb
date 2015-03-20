5f3420e46655c95801be9b92a320b4d7a69f978d
Nicolas Vinet <nvinet@gmail.com>
2015-03-20 11:45:55 +0000

    removing contact form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,6 +1,5 @@
 class ContactController < ApplicationController
   def index
-    @contact = flash[:contact] ? flash[:contact] : Contact.new
     @rooms = ROOMS["rooms"].sort_by {|room| room["priceMin"]}
   end
 end
